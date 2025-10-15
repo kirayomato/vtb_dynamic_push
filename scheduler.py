ed581ca5431d11bba7706e73e1b0a6a239d62843
kirayomato <38139345+kirayomato@users.noreply.github.com>
2025-10-15 15:30:14 +0800

    优化调度器算法

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import time
 from typing import Dict, List, Optional
 from push import global_config as config
+from math import sqrt
 
 
 class Scheduler:
@@ -13,12 +14,16 @@ class Scheduler:
 
     def update(self, key: str, new_weight: int) -> None:
         """更新权重"""
+        old_weight = self.items[key]["weight"]
         if config.get("scheduler", "enable") != "true":
             new_weight = 1
         else:
             max_weight = int(config.get("scheduler", "max_weight") or 10)
-        new_weight = max(1, min(new_weight, max_weight))
-        self.total_weight += new_weight - self.items[key]["weight"]
+            new_weight = max(1, min(new_weight, max_weight))
+            if new_weight > 1:
+                new_weight = sqrt(max(new_weight, max_weight / 2))
+            new_weight = 0.2 * new_weight + 0.8 * old_weight
+        self.total_weight += new_weight - old_weight
         self.items[key]["weight"] = new_weight
 
     def next_target(self) -> Optional[str]:
@@ -28,7 +33,7 @@ class Scheduler:
 
         # 平滑加权轮询算法
         best_item = None
-        max_current_weight = -1
+        max_current_weight = -float("inf")
 
         # 第一步：增加当前权重
         for key, item in self.items.items():
@@ -59,14 +64,15 @@ class Scheduler:
         """更新目标列表：新增/删除目标"""
         new_set = set(new_ids)
         old_set = set(self.items.keys())
-
+        
+        max_weight = int(config.get("scheduler", "max_weight") or 10)
         # 新增
         for key in new_set - old_set:
             self.items[key] = {
-                "weight": 100,  # 权重不能为负数
-                "current_weight": 0,
+                "weight": max_weight,  
+                "current_weight": 100,
             }
-            self.total_weight += 100
+            self.total_weight += max_weight
 
         # 删除
         for key in old_set - new_set:
